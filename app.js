64e6f67d98f63c0cc7c292cf4ba0c821a5e30205
jonnie254 <johnkamanu254@proton.me>
2024-03-12 00:30:28 +0300

    added some code

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ document.getElementById("check-btn").addEventListener("click", function () {
 
 function checkPalindrome(text) {
   let text_new = text.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
-  console.log(text, text_new);
   let len = text_new.length;
   let half_len = Math.floor(len / 2);
   let result = document.getElementById("result");
